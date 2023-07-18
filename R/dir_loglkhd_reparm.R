eb6d0af0f20b75a539540968a831c453eb46e987
Deepak Nag Ayyala <deepaknagayyala@gmail.com>
2023-07-18 16:17:48 -0400

    Modified code
    1. Changed Roxygen headers for parameters
    2. Changed function name to match other functions

---
+++ R/dir_loglkhd_reparm.R
@@ -0,0 +1,21 @@
+#' ver0: Reparameterized Dirichlet distribution
+
+dir.lkhd      <- function(x, mu, theta) {
+    #' @param x Data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet distribution. 
+    #' @param mu A scalar parameter which represents the dispersion parameter of the Dirichlet distribution. 
+    #' @param alpha A $p \times 1$ vector which is the mean parameter of Dirichlet distribution. 
+
+    #' @return lkhd Bichna: Describe the outcome
+
+    ## Check if data and parameter have the same dimension
+    if (ncol(x) != length(alpha)){
+        stop("Data and parameter dimensions do not match")
+    }
+    
+    theta <- exp(alpha)/sum(exp(alpha))
+    n     <- ncol(x)   # sample size of date
+    lkhd  <- n*lgamma(mu) -n*sum(lgamma(mu*theta)) + sum(t(mu*theta-1)%*%log(x))
+    
+    return(lkhd)
+}
+
