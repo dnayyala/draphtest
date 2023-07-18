86e70e07a141eb8df1983918d065b7d1c40ba42f
Deepak Nag Ayyala <deepaknagayyala@gmail.com>
2023-07-18 16:03:28 -0400

    Combined two functions into one Jacobian function
    1. Combined the two functions for alpha and mu into one function by adding the "param" input parameter
    2. Added Roxygen headers

--- R/dir_jacobian.R
+++ R/dir_jacobian.R
@@ -1,39 +1,31 @@
-dir.jacobian <- function(x, mu, alpha){
+dir.jacobian <- function(x, mu, alpha, param = "mu"){
     #' Jacobian of Dirichlet distribution with repect to paramater alpha
-    #' @x is size of (p x n) which is generated from [p]-dimensional Dirichlet distribution. 
-    #' @param mu is constant term. 
-    #' @param alpha is the parameter vector of Dirichlet distribution. 
+    #' @param x A matrix size of (p x n) which is generated from [p]-dimensional Dirichlet distribution. 
+    #' @param mu Scalar parameter of the Dirichlet distribution which represents the dispersion of the data. 
+    #' @param alpha Vector of length p which represents the log-transformed mean vector of Dirichlet distribution. 
+    #" @param param Character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
+    
+    #" @return Bich Na: What is the value returned by the function?
     
     p     <- nrow(x)   # the number of parameters (theta)
+    ## Check to confirm data and parameter vector are of the same dimension
+    if (length(alpha) != p){
+        stop("Data and parameter dimensions do not match.")
+    }
     n     <- ncol(x)   # sample size of x
     theta <- exp(alpha)/sum(exp(alpha))
-    
-    sec.term <- numeric()
-    for(i in 1:p){
-        sec.term[i] <- mu*theta[i]*sum(n*theta[-i]*digamma(mu*theta[-i]) - rowSums(x[-i,]))         
+
+    if (param == "alpha"){
+        sec.term <- numeric(p)
+        for (i in 1:p){
+            sec.term[i] <- mu*theta[i]*sum(n*theta[-i]*digamma(mu*theta[-i]) - rowSums(x[-i,]))         
+        }        
+        jacobian <- mu*theta*(1-theta) *(-n*digamma(mu*theta) + rowSums(x)) + sec.term
+    } else if (param == "mu"){
+        jacobian <- n*digamma(mu) + sum(theta*rowSums(log(x)) - n*theta*digamma(mu*theta))
     }
-    
-    jacobian <- c();
-    jacobian <- mu*theta*(1-theta) *(-n*digamma(mu*theta) + rowSums(x)) + sec.term
     
     return(jacobian);
 }
 
-
-
-dir.mu.jacobian <- function(x, mu, alpha){
-  #' Jacobian of Dirichlet distribution with repect to constant term mu
-  #' @x is size of (p x n) which is generated from [p]-dimensional Dirichlet distribution.
-  #' @param mu is constant term.
-  #' @param alpha is the parameter vector of Dirichlet distribution.
-
-  
-  p     <- nrow(x)   # the number of parameters (theta)
-  n     <- ncol(x)   # sample size of x
-  theta <- exp(alpha)/sum(exp(alpha))
-
-  jacobian.mu <- n*digamma(mu) + sum(theta*rowSums(log(x)) - n*theta*digamma(mu*theta))
-
-  return(jacobian.mu);
-}
 
