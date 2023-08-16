78c05689ff5a6440db58ca326f7225a96fdd3ebb
Binna Choi <136127481+Binna-Choi@users.noreply.github.com>
2023-08-16 18:37:41 -0400

    Update dir_loglkhd_reparm.R
    Minor changes of the log-likelihood function

--- R/dir_loglkhd_reparm.R
+++ R/dir_loglkhd_reparm.R
@@ -1,22 +1,17 @@
-#' ver0: Reparameterized Dirichlet distribution
-
 dir.lkhd      <- function(x, mu, alpha) {
     #' @param x Data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet distribution. 
     #' @param mu A scalar parameter which represents the dispersion parameter of the Dirichlet distribution. 
     #' @param alpha A $p - 1 \times 1$ vector which is the mean parameter of Dirichlet distribution. 
-
-    #' @return lkhd Bichna: Describe the outcome
-
+    #' @return a log-likelihood function of Dirichlet distribution. 
+    
     ## Check if data and parameter have the same dimension
-    if (ncol(x) != length(alpha)){
+    if (nrow(x) != length(alpha)){
         stop("Data and parameter dimensions do not match")
     }
-
-    alpha.full <- c(0, alpha)
-    theta <- exp(alpha.full)/sum(exp(alpha.full))
+    
+    theta <- exp(alpha)/sum(exp(alpha))
     n     <- ncol(x)   # sample size of date
     lkhd  <- n*lgamma(mu) -n*sum(lgamma(mu*theta)) + sum(t(mu*theta-1)%*%log(x))
     
     return(lkhd)
-}
-
+}       
