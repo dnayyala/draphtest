c047a438ff7bd0fc125f8b2d8bb5d3d432fe49d4
Binna Choi <136127481+Binna-Choi@users.noreply.github.com>
2023-08-16 18:39:50 -0400

    Update dir_jacobian.R
    Update the dir_jacobian.R code is based on the new generalized form of Jacobian

--- R/dir_jacobian.R
+++ R/dir_jacobian.R
@@ -4,9 +4,10 @@ dir.jacobian <- function(x, mu, alpha, param = "alpha"){
     #' @param mu Scalar parameter of the Dirichlet distribution which represents the dispersion of the data. 
     #' @param alpha Vector of length p which represents the log-transformed mean vector of Dirichlet distribution. 
     #" @param param Character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
-    #" @return a Jacobian, first-order partial derivatives with respect to param alpha, size of $p$ vector
+    #" @return a Jacobian, the first-order partial derivatives with respect to param alpha, size of $(p-1)$ vector
     
-    p          <- nrow(x)   # the number of parameters (theta
+    p  <- nrow(x)   # the number of parameters
+    
     ## Check to confirm data and parameter vector are of the same dimension
     if (length(alpha) != p){
         stop("Data and parameter dimensions do not match.")
@@ -15,15 +16,20 @@ dir.jacobian <- function(x, mu, alpha, param = "alpha"){
     theta <- exp(alpha)/sum(exp(alpha))
     jacobian <- numeric(nrow(x))
     
- 
+    
     if (param == "alpha"){
-        ident <- matrix(1, ncol = p, nrow = p)
-        diag(ident) <- 0
-            jacobian <- mu*theta*((theta*digamma(mu*theta))%*%ident - (1-theta)*(n*digamma(mu*theta) - rowSums(log(x))))
-            
-    } else if (param == "mu"){
+        
+        Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
+        diag(Q) <- diag(Q)-1
+        Q       <- Q[-1,]
+        W       <- n*digamma(mu*theta)- rowSums(log(x))
+        
+        jacobian <- mu*theta[-1] * (Q%*%W)
+    
+        } else if (param == "mu"){
         jacobian <- n*digamma(mu) + sum(theta*rowSums(log(x)) - n*theta*digamma(mu*theta))
     }
     
     return(jacobian);
 }
+
