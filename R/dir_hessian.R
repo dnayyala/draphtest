3d5681cc53634b4cb28fbd5dabb0149f376e2b27
Binna Choi <136127481+Binna-Choi@users.noreply.github.com>
2023-08-16 18:40:42 -0400

    Update dir_hessian.R
    Update the code is based on the new derivation of the Hessian matrix (generalization and vectorization)

--- R/dir_hessian.R
+++ R/dir_hessian.R
@@ -1,46 +1,49 @@
 dir.hessian<- function(x, mu, alpha, param = "alpha"){
-  #' Hessian matrix of Dirichlet distribution with respect to parameter alpha
-  #' @param x The data matrix of size $p \times n$ which is generated from [p]-dimensional Dirichlet distribution. 
-  #' @param mu A positive scalar parameter which represents the dispersion parameter of the Dirichlet distribution.  
-  #' @param alpha A $p \times 1$ parameter vector which is the log-transformed mean of Dirichlet distribution. 
-  #' @param param A character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
-
-  #" @return Hessian Bich Na: What is the value returned by the function?
-  
-  p <- nrow(x)   # the number of parameters (theta)
-  ## CHeck if data and parameter dimensions match
-  if (length(alpha) != p){
-    stop("Data and parameter dimensions do not match.")
-  }
-  n <- ncol(x)   # sample size of x
-
-  ## Convert the log-mean parameter to mean
-  theta   <- exp(alpha)/sum(exp(alpha))
-
-  if (param == "alpha"){
-    Hessian <- matrix(NA, nrow = p, ncol = p)
+    #' Hessian matrix of Dirichlet distribution with respect to parameter alpha
+    #' @param x The data matrix of size $p \times n$ which is generated from [p]-dimensional Dirichlet distribution. 
+    #' @param mu A positive scalar parameter which represents the dispersion parameter of the Dirichlet distribution.  
+    #' @param alpha A $p \times 1$ parameter vector which is the log-transformed mean of Dirichlet distribution. 
+    #' @param param A character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
+    #" @return the Hessian matrix of Dirichlet distribution size of $(p-1) \times (p-1)$
     
-    #' Off-diagonal elements of Hessian Matrix
-    for(i in 1:p){
-        for(j in 1:p ){
-              Hessian[i,j] <- mu*theta[i]*theta[j]*(
-                  n*(mu*theta[i]*(1-theta[i])*trigamma(mu*theta[i]) + digamma(mu*theta[i])*(1+2*theta[i])
-                    +mu*theta[j]*(1-theta[j])*trigamma(mu*theta[j])+ digamma(mu*theta[j])*(1+2*theta[j])
-                    -sum(theta[c(-i,-j)]^2*mu*trigamma(mu*theta[c(-i,-j)]) - 2*digamma(mu*theta[c(-i,-j)])))
-                  +sum(log(x[i,])*(2*theta[i]-1) + log(x[j,])*(2*theta[j]-1) +2*sum(theta[c(-i,-j)]*log(x[c(-i,-j),]))))
-    } }
+    p <- nrow(x)   # the number of parameters (theta)
     
-    #' Diagonal elements of Hessian Matrix
-    for(i in 1:p){
-        Hessian[i,i] <- mu*theta[i]*
-            (n*((1-theta[i])*(digamma(mu*theta[i])*(1+mu*theta[i]) - mu*theta[i]*trigamma(mu*theta[i]))
-                - sum(theta[-i]*(mu*trigamma(mu*theta[-i])*theta[i]*theta[-i]-digamma(mu*theta[-i]))))
-             + sum((1-theta[i])*(1-2*theta[i])*log(x[i,]) - colSums(theta[-i]*log(x[-i,]))))
+    ## Check if data and parameter dimensions match
+    if (length(alpha) != p){
+        stop("Data and parameter dimensions do not match.")
     }
-  } else if (param == "mu"){
-    Hessian   <- matrix(NA, nrow = p-1, ncol = p-1)  ## matrix(NA, nrow = p, ncol = p) 
-    Hessian    <- n*(trigamma(mu) - sum(theta^2 * trigamma(mu*theta) ))
-  }
+    n <- ncol(x)   # sample size of x
+    
+    ## Convert the log-mean parameter to mean
+    theta   <- exp(alpha)/sum(exp(alpha))
+    hessian <- matrix(NA, nrow = p-1, ncol = p-1)
+    if (param == "alpha"){
+       
+        q       <- diag(1, p)
+        W       <- n*digamma(mu*theta) - rowSums(log(x))
+        Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
+        diag(Q) <- diag(Q)-1
+        Q       <- Q[-1,]  
+        
+        #' Off-diagonal elements of Hessian Matrix
+        for(k in 2:p){
+            for(l in 2:p){
+                t_kl <- theta - q[k,] - q[l,]
+                hessian[k-1,l-1] <- mu*theta[k]*theta[l]*(-n*mu*((theta*t_kl)%*%trigamma(mu*theta))
+                                    + (2*theta - q[k,] - q[l,]) %*% W )
+            }
+        }
+        
 
-  return(Hessian);
+     
+        #' Diagonal elements of Hessian Matrix
+         diag(hessian) <-mu*theta[-1]*(-n*mu*theta[-1]*(Q^2 %*% trigamma(mu*theta)) + (1- 2*theta[-1])*(Q%*%W))
+
+    } else if (param == "mu"){
+        hessian   <- matrix(NA, nrow = p-1, ncol = p-1)  ## matrix(NA, nrow = p, ncol = p) 
+        hessian    <- n*(trigamma(mu) - sum(theta^2 * trigamma(mu*theta) ))
+    }
+    
+    return(hessian);
 }
+
