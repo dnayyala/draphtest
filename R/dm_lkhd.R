1dbfdb657a255b6282a5c9a562b3d80b413aea9d
Binna Choi <136127481+Binna-Choi@users.noreply.github.com>
2023-08-31 11:19:25 -0400

    Add dm_lkhd.R file via upload
    R code for the log-likelihood function of the Dirichlet-multinomial distribution

---
+++ R/dm_lkhd.R
@@ -0,0 +1,18 @@
+dm.lkhd <- function(x, mu, alpha) {
+    #' @param x Data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet-multinomial  distribution. 
+    #' @param mu A scalar parameter which represents the dispersion parameter of the distribution. 
+    #' @param alpha A $p - 1 \times 1$ vector which is the mean parameter of the distribution. 
+    #' @return a scalar which is the log-likelihood of the Dirichlet-multinomial distribution. 
+    
+    ## Check if data and parameter have the same dimension
+    if (nrow(x) != length(alpha)){
+        stop("Data and parameter dimensions do not match")
+    }
+    
+    n       <- ncol(x)   # sample size of date
+    theta   <- exp(alpha)/sum(exp(alpha))
+    X.plus  <- colSums(x)
+    lkhd    <- sum(lgamma(mu) + lgamma(X.plus+1) - lgamma(X.plus+mu) + colSums(lgamma(x + mu*theta) - lgamma(mu * theta) - lgamma(x+1)))
+    
+    return(lkhd)
+}
