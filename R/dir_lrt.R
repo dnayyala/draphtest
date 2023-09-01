e016130d435311cfd804df1a010a1c949f8d0880
Binna Choi <136127481+Binna-Choi@users.noreply.github.com>
2023-09-01 17:54:48 -0400

    Update dir_lrt.R
    Added explanation of the code

--- R/dir_lrt.R
+++ R/dir_lrt.R
@@ -1,5 +1,14 @@
 dir.lrt <- function(x, y = NULL, mu.x, mu.y = NULL, alpha.x, alpha.y = NULL, alpha , type = "one"){
-    
+    #' Likelihood-ratio test to parameter alpha
+    #' @parm x A data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet distribution. 
+    #' @parm y A data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet distribution. 
+    #' @parm mu.x A scalar parameter of x which represents the dispersion parameter of the Dirichlet distribution
+    #' @parm mu.y A scalar parameter of y which represents the dispersion parameter of the Dirichlet distribution
+    #' @param alpha Vector of length p which represents the log-transformed mean vector under the null hypothesis.
+    #' @param alpha.x Vector of length p which represents the log-transformed mean vector of x.  
+    #' @param alpha.y Vector of length p which represents the log-transformed mean vector of y. 
+    #' @param type If ```type``` is  set as ```one```, the function will return a one sample test result, else if ```type``` is  set as ```two``` then the function will return two samples test result.
+    #' @return the function will return test statistic, p value and the result of the Likelihood-ratio test.
     p <- length(alpha) # the number of parameters
     
     if (is.null(mu.y)){
