a4bb8b97da0b09fcfb95a318dae829fe42529bdf
Binna Choi <136127481+Binna-Choi@users.noreply.github.com>
2023-12-01 20:11:10 +0100

    Update dm_nr.R
    Minor Updates (the initial value of mu is changed to MME of mu)

--- R/dm_nr.R
+++ R/dm_nr.R
@@ -1,21 +1,18 @@
 dm.nr <- function(x, mu = NULL, alpha = NULL, tol = 1e-4, maxiters = 1e2){
     #' Newton-Rapshon algorithm to estimate parameter alpha (or theta)
-    #' @parm x A data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet distribution. 
-    #' @param mu A scalar parameter which represents the dispersion parameter of the Dirichlet distribution
-    #' @param alpha Vector of length p which represents the log-transformed mean vector of Dirichlet distribution. 
+    #' @parm x A data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet-multinomial distribution. 
+    #' @param mu A scalar parameter which represents the dispersion parameter of the distribution
+    #' @param alpha Vector of length p which represents the log-transformed mean vector of the distribution. 
     #' @param tol Tolerance cutoff for determining convergence of the Newton-Raphson algorithm
     #' @param maxiters Maximum number of iterations for the algorithm. Default is set at 100.
     #' @param param Character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
     #' @return the function will return a $p \times 1$ vector containing the estimate of the param alpha and a the estimate of mu
-    
-   
     p      <- nrow(x)
     X.plus <- colSums(x)
     N      <- mean(X.plus)  # Total sum 
     
     # Initiate the values         
     alpha1 <- list();
-    step.size.vector  <- c();
     theta1      <- rowMeans(x)/N
     
     if (is.null(alpha)){
@@ -28,11 +25,20 @@ dm.nr <- function(x, mu = NULL, alpha = NULL, tol = 1e-4, maxiters = 1e2){
     if (length(alpha1[[1]]) != p){
         stop("Data and parameter dimensions do not match")
     }
+    # mu1 <- c()        
+    # # Initiate the values 
+    # if (is.null(mu)){
+    #     mu1[1] <-   mean(x)
+    # } else {
+    #     mu1[1] <- mu
+    # }
+    
     mu1 <- c()        
     # Initiate the values 
     if (is.null(mu)){
-        theta  <- exp(alpha1[[1]])/sum(exp(alpha1[[1]]))        
-        mu1[1] <-   mean(rowMeans(x))
+        var.x <- apply(x, 1, var)
+        mu.hat <- (theta1*N*(N-theta1) - var.x)/(var.x - theta1*N*(1-theta1))
+        mu1[1] <-  mean(mu.hat)
     } else {
         mu1[1] <- mu
     }
@@ -42,18 +48,24 @@ dm.nr <- function(x, mu = NULL, alpha = NULL, tol = 1e-4, maxiters = 1e2){
     
     n.step   <- 1; 
     eps      <- 1e2;   # Difference in function between iterations
+    step.size.vector  <- c();
     
     while (eps > tol & n.step <= maxiters){
         n.step    <- n.step + 1;
        
-        
         ## Fix mu and update alpha first 
-        Q  <- solve(dm.hessian(x, mu=mu1[n.step-1], alpha=alpha1[[n.step-1]], param = "alpha"), dm.jacobian(x, mu=mu1[n.step-1], alpha=alpha1[[n.step-1]], param = "alpha"))
+        Q  <- solve(dm.hessian(x, mu=as.numeric(mu1[n.step-1]), alpha=alpha1[[n.step-1]], param = "alpha"), dm.jacobian(x, mu=as.numeric(mu1[n.step-1]), alpha=alpha1[[n.step-1]], param = "alpha"))
         ## Check if updated estimate is increasing the likelihood and not decreasing.        
         step.size <- 1
         step.size.check <- TRUE
         while (step.size.check && step.size >= 1e-6){
             alpha1[[n.step]] <-  c(0, alpha1[[n.step -1]][2:p] - step.size*Q);
+            
+            dm.lkhd.test <- dm.lkhd(x, mu=as.numeric(mu1[n.step-1]), alpha=alpha1[[n.step]])
+            if(is.nan(dm.lkhd.test))
+            {
+                alpha1[[n.step]] <- alpha1[[n.step - 1]]
+            }
             step.size.check <- ((dm.lkhd(x, mu=as.numeric(mu1[n.step-1]), alpha=alpha1[[n.step]]) - dm.lkhd(x, mu=as.numeric(mu1[n.step-1]), alpha=alpha1[[n.step-1]])) <= 0)
             step.size <- step.size*1e-1
         }
@@ -61,8 +73,9 @@ dm.nr <- function(x, mu = NULL, alpha = NULL, tol = 1e-4, maxiters = 1e2){
             alpha1[[n.step]] <- alpha1[[n.step - 1]]
         }
         
+
         ## Fix alpha and update mu
-        W  <- dm.jacobian(x, mu=mu1[n.step - 1], alpha=alpha1[[n.step]], param = "mu")/dm.hessian(x, mu=as.numeric(mu1[n.step - 1]), alpha=alpha1[[n.step]], param = "mu")
+        W  <- dm.jacobian(x, mu=as.numeric(mu1[n.step - 1]), alpha=alpha1[[n.step]], param = "mu")/dm.hessian(x, mu=as.numeric(mu1[n.step - 1]), alpha=alpha1[[n.step]], param = "mu")
         ## Check if updated estimate is positive for different step sizes.        
         step.size <- 1
         step.size.check <- 10^(-(1:6))[(sapply(1:6, \(x) as.numeric(mu1[n.step -1]) - (10^(-x))*W) >= 0)]
