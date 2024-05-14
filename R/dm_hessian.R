4d5075ce11875e70fd5e5daf2090a403c38a194c
Binnah <bnchoi.st@gamil.com>
2024-05-14 23:49:58 +0900

    Updated the Hessian and Jacobian of the Dirichlet-multinomial distribution (Cleaned version)

--- R/dm_hessian.R
+++ R/dm_hessian.R
@@ -1,47 +1,47 @@
-dm.hessian<- function(x, mu, alpha, param = "alpha"){
-    #' Hessian matrix of Dirichlet distribution with respect to parameter alpha
-    #' @param x The data matrix of size $p \times n$ which is generated from [p]-dimensional Dirichlet distribution. 
-    #' @param mu A positive scalar parameter which represents the dispersion parameter of the Dirichlet distribution.  
-    #' @param alpha A $p \times 1$ parameter vector which is the log-transformed mean of Dirichlet distribution. 
-    #' @param param A character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
-    #' @return If ```param``` is set as ```alpha```, the function will return a $(p-1) \times (p-1)$ Hessian matrix, else if ```param``` is set as ```mu```, the function will return a scalar of Hessian
+dm.hessian <- function(x, mu, alpha, param = "alpha"){
+  #' Hessian matrix of Dirichlet distribution with respect to parameter alpha
+  #' @param x The data matrix of size $p \times n$ which is generated from [p]-dimensional Dirichlet distribution. 
+  #' @param mu A positive scalar parameter which represents the dispersion parameter of the Dirichlet distribution.  
+  #' @param alpha A $p \times 1$ parameter vector which is the log-transformed mean of Dirichlet distribution. 
+  #' @param param A character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
+  #' @return If ```param``` is set as ```alpha```, the function will return a $(p-1) \times (p-1)$ Hessian matrix, else if ```param``` is set as ```mu```, the function will return a scalar of Hessian
+  
+  p <- nrow(x)   # the number of parameters (theta)
+  
+  ## Check if data and parameter dimensions match
+  if (length(alpha) != p){
+    stop("Data and parameter dimensions do not match.")
+  }
+  n <- ncol(x)   # sample size of x
+  
+  ## Convert the log-mean parameter to mean
+  theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
+  hessian <- matrix(NA, nrow = p-1, ncol = p-1)
+  if (param == "alpha"){
     
-    p <- nrow(x)   # the number of parameters (theta)
+    I       <- diag(1, p)
+    m       <- rowSums(digamma(x + mu*theta)) - n*digamma(mu*theta)
+    b       <- rowSums(trigamma(x + mu*theta)) - n*trigamma(mu*theta)
+    Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
+    diag(Q) <- diag(Q)-1
+    Q       <- Q[-1,]  
     
-    ## Check if data and parameter dimensions match
-    if (length(alpha) != p){
-        stop("Data and parameter dimensions do not match.")
-    }
-    n <- ncol(x)   # sample size of x
+    #' Off-diagonal elements of Hessian Matrix
+    for(u in 2:p){
+      for(v in 2:p){
+        e_uv <- I[u,] + I[v, ]
+        hessian[u-1,v-1] <- mu*theta[u]*theta[v]*((2*theta - e_uv)%*%m + mu*(t(theta*(theta-e_uv)) %*% b))
+        
+      }}
     
-    ## Convert the log-mean parameter to mean
-    theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
-    hessian <- matrix(NA, nrow = p-1, ncol = p-1)
-    if (param == "alpha"){
-        
-        q       <- diag(1, p)
-        m       <- rowSums(digamma(x + mu*theta)) - n*digamma(mu*theta)
-        v       <- rowSums(trigamma(x + mu*theta)) - n*trigamma(mu*theta)
-        Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
-        diag(Q) <- diag(Q)-1
-        Q       <- Q[-1,]  
-        
-        #' Off-diagonal elements of Hessian Matrix
-        for(k in 2:p){
-            for(l in 2:p){
-                #hessian[k-1,l-1] <- -mu*theta[k]*theta[l]*((theta - q[k,])%*% m -(theta - q[l,])%*%(m + mu*(theta - q[k,])*v))
-                hessian[k-1,l-1] <- mu*theta[k]*theta[l]*((2*theta - q[k,] - q[l,])%*%m + 
-                        mu*((theta*(theta - q[k,] - q[l,])) %*%v))
-            }}
-        
-            
-        #' Diagonal elements of Hessian Matrix
-        diag(hessian) <- mu* theta[-1]*((2*theta[-1]-1)*(Q %*% m) + mu* theta[-1]*(Q^2 %*% v))
-        
-        
-    } else if (param == "mu"){
-        hessian    <- n*(trigamma(mu) - sum(theta^2 * trigamma(mu*theta) ))
-    }
     
-    return(hessian);
+    #' Diagonal elements of Hessian Matrix
+    diag(hessian) <- -mu*theta[-1]*((1-2*theta[-1])*(Q%*%m) - mu*theta[-1]*(Q^2%*%b))
+    
+    
+  } else if (param == "mu"){
+    hessian    <- n*(trigamma(mu) - trigamma(X.plus + mu)+ colSums(theta^2 * trigamma(x+mu*theta) -theta^2 * trigamma(mu*theta) ))
+  }
+  
+  return(hessian);
 }
--- R/dm_jacobian.R
+++ R/dm_jacobian.R
@@ -1,38 +1,39 @@
 dm.jacobian <- function(x, mu, alpha, param = "alpha"){
-    #' Jacobian of Dirichlet distribution with respect to parameter alpha
-    #' @param x A matrix size of (p x n) which is generated from [p]-dimensional Dirichlet distribution. 
-    #' @param mu Scalar parameter of the Dirichlet distribution which represents the dispersion of the data. 
-    #' @param alpha Vector of length p which represents the log-transformed mean vector of Dirichlet distribution. 
-    #' @param param Character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
-    #' @return If ```param``` is set as ```alpha```, the function will return a $(p-1) \times 1$ vector of Jacobian, else if ```param``` is set as ```mu```, the function will return a scalar of Jacobian.
+  #' Jacobian of Dirichlet distribution with respect to parameter alpha
+  #' @param x A matrix size of (p x n) which is generated from [p]-dimensional Dirichlet distribution. 
+  #' @param mu Scalar parameter of the Dirichlet distribution which represents the dispersion of the data. 
+  #' @param alpha Vector of length p which represents the log-transformed mean vector of Dirichlet distribution. 
+  #' @param param Character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
+  #' @return If ```param``` is set as ```alpha```, the function will return a $(p-1) \times 1$ vector of Jacobian, else if ```param``` is set as ```mu```, the function will return a scalar of Jacobian.
+  
+  n  <- ncol(x)   # sample size of date
+  p  <- nrow(x)
+  
+  theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
+  X.plus  <- colSums(x)
+  
+  ## Check to confirm data and parameter vector are of the same dimension
+  if (length(alpha) != p){
+    stop("Data and parameter dimensions do not match.")
+  }
+  
+  jacobian <- numeric(nrow(x))
+  
+  
+  if (param == "alpha"){
+    #' Jacobian in terms of parameter alpha
+    Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
+    diag(Q) <- diag(Q)-1
+    Q       <- Q[-1,]
+    m       <- rowSums(digamma(x + mu*theta)) - n*digamma(mu*theta)
     
-    n  <- ncol(x)   # sample size of date
-    p  <- nrow(x)
+    jacobian <- - mu*theta[-1] * (Q%*%m)
     
-    theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
-    X.plus  <- colSums(x)
-    
-    ## Check to confirm data and parameter vector are of the same dimension
-    if (length(alpha) != p){
-        stop("Data and parameter dimensions do not match.")
-    }
-
-    jacobian <- numeric(nrow(x))
-    
-    
-    if (param == "alpha"){
-        #' Jacobian in terms of parameter alpha
-        Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
-        diag(Q) <- diag(Q)-1
-        Q       <- Q[-1,]
-        m       <- rowSums(digamma(x + mu*theta)) - n*digamma(mu*theta)
-        
-        jacobian <- mu*theta[-1] * (Q%*%m)
-        
-    } else if (param == "mu"){
-        #' Jacobian in terms of parameter mu
-        jacobian <- sum(digamma(mu)-digamma(X.plus+mu) + colSums(theta* digamma(x + mu*theta) - theta*digamma(mu*theta)))
-    }
-    
-    return(jacobian);
+  } else if (param == "mu"){
+    #' Jacobian in terms of parameter mu
+    jacobian <- sum(digamma(mu)-digamma(X.plus+mu) + colSums(theta*digamma(x + mu*theta) - theta*digamma(mu*theta)))
+  }
+  
+  return(jacobian);
 }
+
