1a2dc2968ec3cc35cbfcedffd3fb61fd08fd5c8a
Binnah <bnchoi.st@gamil.com>
2024-05-14 23:40:45 +0900

    Update the Hessian and Jacobian of Dirichlet distribution (Cleaned version)

--- R/dir_hessian.R
+++ R/dir_hessian.R
@@ -1,50 +1,52 @@
-dir.hessian<- function(x, mu, alpha, param = "alpha"){
-    #' Hessian matrix of Dirichlet distribution with respect to parameter alpha
-    #' 
-    #' @param x The data matrix of size $p \times n$ which is generated from [p]-dimensional Dirichlet distribution. 
-    #' @param mu A positive scalar parameter which represents the dispersion parameter of the Dirichlet distribution.  
-    #' @param alpha A $p \times 1$ parameter vector which is the log-transformed mean of Dirichlet distribution. 
-    #' @param param A character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
-    #" @return the Hessian matrix of Dirichlet distribution size of $(p-1) \times (p-1)$
+dir.hessian <- function(x, mu, alpha, param = "alpha"){
+  #' Hessian matrix of Dirichlet distribution with respect to parameter alpha
+  #' 
+  #' @param x The data matrix of size $p \times n$ which is generated from [p]-dimensional Dirichlet distribution. 
+  #' @param mu A positive scalar parameter which represents the dispersion parameter of the Dirichlet distribution.  
+  #' @param alpha A $p \times 1$ parameter vector which is the log-transformed mean of Dirichlet distribution. 
+  #' @param param A character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
+  #" @return the Hessian matrix of Dirichlet distribution size of $(p-1) \times (p-1)$
+  
+  p <- nrow(x)   # the number of parameters (theta)
+  
+  #print(p) 
+  
+  ## Check if data and parameter dimensions match
+  if (length(alpha) != p){
+    stop("Data and parameter dimensions do not match.")
+  }
+  n <- ncol(x)   # sample size of x
+  
+  ## Convert the log-mean parameter to mean
+  theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
+  hessian <- matrix(NA, nrow = p-1, ncol = p-1)
+  if (param == "alpha"){
     
-    p <- nrow(x)   # the number of parameters (theta)
+    I       <- diag(1, p)
+    W       <- rowSums(log(x)) - n*digamma(mu*theta)
+    Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
+    diag(Q) <- diag(Q)-1
+    Q       <- Q[-1,]  
     
-    #print(p) 
-    
-    ## Check if data and parameter dimensions match
-    if (length(alpha) != p){
-        stop("Data and parameter dimensions do not match.")
-    }
-    n <- ncol(x)   # sample size of x
-    
-    ## Convert the log-mean parameter to mean
-    theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
-    hessian <- matrix(NA, nrow = p-1, ncol = p-1)
-    if (param == "alpha"){
+    u <- 2; v <- 3
+    #' Off-diagonal elements of Hessian Matrix
+    for(u in 2:p){
+      for(v in 2:p){
+        e_uv <- I[u,] + I[v,]
+        t_kl <- 
+          hessian[u-1,v-1] <- mu*theta[u]*theta[v]*((2*theta - e_uv)%*%W - n*mu*(t(theta*(theta-e_uv)) %*% trigamma(mu*theta)))
         
-        q       <- diag(1, p)
-        W       <- n*digamma(mu*theta) - rowSums(log(x))
-        Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
-        diag(Q) <- diag(Q)-1
-        Q       <- Q[-1,]  
-        
-        #' Off-diagonal elements of Hessian Matrix
-        for(k in 2:p){
-            for(l in 2:p){
-                t_kl <- theta - q[k,] - q[l,]
-                hessian[k-1,l-1] <- mu*theta[k]*theta[l]*(-n*mu*((theta*t_kl)%*%trigamma(mu*theta))
-                                                          + (2*theta - q[k,] - q[l,]) %*% W )
-            }
-        }
-        
-        
-        
-        #' Diagonal elements of Hessian Matrix
-        diag(hessian) <-mu*theta[-1]*(-n*mu*theta[-1]*(Q^2 %*% trigamma(mu*theta)) + (1- 2*theta[-1])*(Q%*%W))
-        
-    } else if (param == "mu"){
-        hessian    <- n*(trigamma(mu) - sum(theta^2 * trigamma(mu*theta) ))
+      }
     }
     
-    return(hessian);
+    
+    #' Diagonal elements of Hessian Matrix
+    diag(hessian) <- -mu*theta[-1]*((1-2*theta[-1])*(Q%*%W) + n*mu*theta[-1]*(Q^2%*%trigamma(mu*theta)))
+    #mu*theta[-1]*(-n*mu*theta[-1]*(Q^2 %*% trigamma(mu*theta)) + (1- 2*theta[-1])*(Q%*%W))
+    
+  } else if (param == "mu"){
+    hessian    <- n*(trigamma(mu) - sum(theta^2 * trigamma(mu*theta)))
+  }
+  
+  return(hessian);
 }
--- R/dir_jacobian.R
+++ R/dir_jacobian.R
@@ -1,35 +1,35 @@
 dir.jacobian <- function(x, mu, alpha, param = "alpha"){
-    #' Jacobian of Dirichlet distribution with respect to parameter alpha
-    #' 
-    #' @param x A matrix size of (p x n) which is generated from [p]-dimensional Dirichlet distribution. 
-    #' @param mu Scalar parameter of the Dirichlet distribution which represents the dispersion of the data. 
-    #' @param alpha Vector of length p which represents the log-transformed mean vector of Dirichlet distribution. 
-    #' @param param Character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
-    #' @return a Jacobian, the first-order partial derivatives with respect to param alpha, size of $(p-1)$ vector
+  #' Jacobian of Dirichlet distribution with respect to parameter alpha
+  #' 
+  #' @param x A matrix size of (p x n) which is generated from [p]-dimensional Dirichlet distribution. 
+  #' @param mu Scalar parameter of the Dirichlet distribution which represents the dispersion of the data. 
+  #' @param alpha Vector of length p which represents the log-transformed mean vector of Dirichlet distribution. 
+  #' @param param Character string identifying the parameter for which the Jacobian is being computed. The possible values are "mu" or "alpha".
+  #' @return a Jacobian, the first-order partial derivatives with respect to param alpha, size of $(p-1)$ vector
+  
+  p  <- nrow(x)   # the number of parameters
+  
+  ## Check to confirm data and parameter vector are of the same dimension
+  if (length(alpha) != p){
+    stop("Data and parameter dimensions do not match.")
+  }
+  
+  n     <- ncol(x)   # sample size of x
+  theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
+  jacobian <- numeric(nrow(x))
+  
+  Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
+  diag(Q) <- diag(Q)-1
+  Q       <- Q[-1,]
+  W       <- rowSums(log(x)) - n*digamma(mu*theta) 
+  
+  if (param == "alpha"){
     
-    p  <- nrow(x)   # the number of parameters
+    jacobian <- -mu*theta[-1] * (Q%*%W)
     
-    ## Check to confirm data and parameter vector are of the same dimension
-    if (length(alpha) != p){
-        stop("Data and parameter dimensions do not match.")
-    }
-    n     <- ncol(x)   # sample size of x
-    theta <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
-    jacobian <- numeric(nrow(x))
-    
-    
-    if (param == "alpha"){
-        
-        Q       <- matrix(theta, nrow=p, ncol=p, byrow=TRUE)
-        diag(Q) <- diag(Q)-1
-        Q       <- Q[-1,]
-        W       <- n*digamma(mu*theta)- rowSums(log(x))
-        
-        jacobian <- mu*theta[-1] * (Q%*%W)
-    
-        } else if (param == "mu"){
-        jacobian <- n*digamma(mu) + sum(theta*rowSums(log(x)) - n*theta*digamma(mu*theta))
-    }
-    
-    return(jacobian);
+  } else if (param == "mu"){
+    jacobian <- n*digamma(mu) + theta%*%W
+  }
+  
+  return(jacobian);
 }
