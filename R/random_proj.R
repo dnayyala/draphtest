ad29a4e0962830cbf8aa714b0e7eb0090e31da2a
Binnah <bnchoi.st@gamil.com>
2024-05-27 13:00:36 +0900

    Add the proposed random projection matrix function

---
+++ R/random_proj.R
@@ -0,0 +1,21 @@
+
+random.proj <- function(nrow, ncol, seed=NULL){ 
+# Random matrix generator for Dirichlet distribution
+# 
+#' @param nrow Number of rows in the random matrix to be generated
+#' @param ncol Number of columns
+#' @param method The method to be used for generating elements in the matrix. If   
+
+
+  R <- matrix(NA, k, p)
+  
+  repeat{
+    for (j in 1:p){
+      R[,j]<- rmultinom(1, 1, prob = rep(1/k, k))
+    }
+    if(qr(R)$rank == k){   
+      break
+    }} 
+  return(R)
+}
+
