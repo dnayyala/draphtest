6c4e63e52ee7860603451f68f7f0e4b67669e534
Binnah <bnchoi.st@gamil.com>
2024-05-27 18:39:01 +0900

    Update the simluation setup1 (One sample empirical alpha value in Dirichlet)

--- Simulation/simulation_setup1 (draft).R
+++ Simulation/simulation_setup1 (draft).R
@@ -1,13 +1,14 @@
+# Simulation Set-Up 1: Simulation for empirical type 1 error of One sample test (Dirichlet distribution)
 
 ####################################################
 ##############         LIBRARY      ################
 ####################################################
 rm(list=ls())
-setwd("/Users/bchoi/Google Drive/Projects/Random Project/Reparameter/R codes/New theta/Dirichlet Multinomial")
+setwd("/Users/bchoi/NewFolder/draphtest/R")
+#setwd("~path")
+
 #args <- commandArgs(trailingOnly = TRUE)
 ## args will be a vector of length three containing p n and k
-library(dirmult)
-
 
 # R packages
 library(mvtnorm)
@@ -16,45 +17,35 @@ library(doParallel)
 library(doRNG)
 library(gtools)
 library(ICSNP)
-start.time <- Sys.time()
-source("dm_lkhd.R")
-source("dm_jacobian.R") # Jacobian with constant term (mu)
-source("dm_hessian.R")
-source("dm_nr.R")
+
+source("dir_lkhd.R")
+source("dir_hessian.R")
+source("dir_jacobian.R")
+source("dir_nr.R")
 source("random_proj.R")
-source("dm_wald_test.R")
-source("dm_lrt.R")
+source("dir_wald_test.R")
+source("dir_lrt.R")
 source("ortho_randproj.R")
 
+
+start.time <- Sys.time()
 ## Set values
-p        <- 100 #as.integer(args[1])
-n        <- 20 ##as.integer(args[2])    # the number of samples from Dirichlet distribution
-k        <- 10 #as.integer(args[3])
+p            <- 100
+n            <- 50 # the number of samples from Dirichlet distribution
+k            <- 10 #as.integer(args[1])
+type1.err    <- 0.05   # Significance level
 
 # Generate Dirichlet random variables
 set.seed(1234)
-mu     <- 10
-alpha  <- c(0, rgamma(p-1, 3, 2))
+mu         <- 100 #as.integer(args[2])
+
+alpha      <- c(0, rgamma(p-1, 3, 5))
 theta.null <- exp(alpha - max(alpha))/sum(exp(alpha - max(alpha)))
 
-type1err = 0.05
-
-
-# Generate Dirichlet-multinomial distribution
-lambda <- 100*p  #1e4 # empirical type 1 error (alpha) is related to the lambda value
-x <- matrix(NA, p, n)
-
-X.plus <- rpois(n, lambda) # X+ in the manuscript
-for(i in 1:n){
-    prob <- rdirichlet(1, mu*theta.null)
-    x[,i]  <- rmultinom(1, size = X.plus[i], prob)
-}
-
-
 ## STAGE 1
-n.boots  <- 1e2
-m.random <- 1e2
-n.total  <- 1e2
+n.boots  <- 1e3
+m.random <- 1e3
+n.total  <- 1e3
 
 n.cores <- detectCores()*0.75
 registerDoParallel(cores=n.cores)
@@ -64,110 +55,87 @@ p.value.wald  <- numeric(m.random)
 p.value.lrt   <- numeric(m.random)
 p.value.raptt <- numeric(m.random)
 
-adj.func <- function(x, X.plus){
-    n <- length(X.plus)
-    output <- matrix(NA, nrow(x), n)
-    for(i in 1:n){
-        output[,i] <- x[,i]/X.plus[i]
-    }
-    return(output)
-}
-
 
 avg.p.value = foreach(i=1:n.boots, .packages=c('gtools', 'ICSNP'), .combine='rbind') %dorng%  {
-    x.boot <- matrix(NA, p, n)
-    for(i in 1:n){
-        prob       <- rdirichlet(1, mu*theta.null)
-        x.boot[,i] <- rmultinom(1, size = X.plus[i], prob)
-    }
-
-    for(m in 1:m.random){
-        RP.orth   <- ortho.randproj(nrow=k, ncol=p, method = "norm", seed = NULL) # Random projection method using orthogonal for RAPTT
-        RP.prop   <- random.proj(p, k) # The proposed random projection method for Wald and LRT
-        
-        rx.dirmult <- RP.prop %*% x.boot
-        raw.raptt  <- (RP.orth %*% x.boot) 
-        rx.raptt   <- adj.func(raw.raptt, X.plus) # fix the projected data 
-
-        
-        r.null.theta   <- c(RP.prop %*% theta.null)
-        r.null.dirmult <- log(r.null.theta) - log(r.null.theta[1]);
-        r.null.raptt   <- RP.orth %*% theta.null
-   
-        ## estimate parameter with the projected data
-        parm.est  <- dm.nr(rx.dirmult)
-        alpha.est <- unlist(parm.est$alpha)
-        r.mu.boot <- parm.est$mu
-        
-        p.value.wald[m]  <- dm.wald(x=rx.dirmult, mu.x = r.mu.boot, alpha.null = r.null.dirmult, alpha.x=alpha.est)$p.value
-        p.value.lrt[m]   <- dm.lrt(x=rx.dirmult, mu.x = r.mu.boot, alpha.null = r.null.dirmult, alpha.x=alpha.est)$p.value
-        p.value.raptt[m] <- HotellingsT2(t(rx.raptt), mu = r.null.raptt, test = "chi")$p.value
-    }
-    c(mean(p.value.wald), mean(p.value.lrt), mean(p.value.raptt))
+  x.boot  <- t(rdirichlet(n, mu*theta.null))
+  
+  for(m in 1:m.random){
+    RP.orth   <- ortho.randproj(nrow=k, ncol=p, method = "norm", seed = NULL) # Random projection method using orthogonal for RAPTT
+    RP.prop   <- random.proj(p, k) # The proposed random projection method for Wald and LRT
+    
+    rx.dir    <- RP.prop %*% x.boot
+    rx.raptt  <- RP.orth %*% x.boot
+    
+    r.null.theta <- RP.prop %*% theta.null
+    r.null.dir   <- log(r.null.theta) - log(r.null.theta[1]);
+    r.null.raptt  <- RP.orth %*% theta.null
+    
+    ## estimate parameter with the projected data
+    parm.est   <- dir.nr(rx.dir)
+    alpha.est  <- unlist(parm.est$alpha)
+    r.mu.boot  <- parm.est$mu
+    
+    p.value.wald[m]  <- dir.wald(x=rx.dir, mu.x = r.mu.boot, alpha = r.null.dir, alpha.x=alpha.est)$p.value
+    p.value.lrt[m]   <- dir.lrt(x=rx.dir, mu.x = r.mu.boot, alpha = r.null.dir, alpha.x=alpha.est)$p.value
+    p.value.raptt[m] <- HotellingsT2(t(rx.raptt), mu = r.null.raptt, test = "chi")$p.value
+  }
+  c(mean(p.value.wald), mean(p.value.lrt), mean(p.value.raptt))
 }
-
-
-## avg.p.value is a matrix of size n.boots x 3
 
 func.cutoff <- function(x){
-    quantile(x, type1err)
+  quantile(x, type1.err)
 }
+
 cut.off <- apply(avg.p.value, 2, func.cutoff)
 
 # STEP 2
-#emp.alpha <- numeric(length(mu.Y))
 p.value <- numeric(m.random)
-mean.p <- numeric(n.total)
+mean.p  <- numeric(n.total)
 
 mean.p = foreach(i=1:n.total, .packages=c('gtools', 'ICSNP'), .combine='rbind') %dorng%  {
+  
+  x.boot  <- t(rdirichlet(n, mu*theta.null))
+  
+  for(m in 1:m.random){
     
-    x.boot <- matrix(NA, p, n)
-    for(i in 1:n){
-        prob        <- rdirichlet(1, mu*theta.null)
-        x.boot[,i]  <- rmultinom(1, size = X.plus[i], prob)
-    }
+    RP.orth   <- ortho.randproj(nrow=k, ncol=p, method = "norm", seed = NULL) # Random projection method using orthogonal for RAPTT
+    RP.prop   <- random.proj(p, k) # The proposed random projection method for Wald and LRT
     
-    for(m in 1:m.random){
-        RP.orth   <- ortho.randproj(nrow=k, ncol=p, method = "norm", seed = NULL) # Random projection method using orthogonal for RAPTT
-        RP.prop   <- random.proj(p, k)                                            # The proposed random projection method for Wald and LRT
-        
-        rx.dirmult  <- RP.prop %*% x.boot
-        raw.raptt   <- (RP.orth %*% x.boot) 
-        rx.raptt    <- adj.func(raw.raptt, X.plus) # fix the projected data 
-        
-        r.null.theta   <- c(RP.prop %*% theta.null)
-        r.null.dirmult <- log(r.null.theta) - log(r.null.theta[1]);
-        r.null.raptt   <- RP.orth %*% theta.null
-        
-        ## estimate parameter with the projected data
-        parm.est  <- dm.nr(rx.dirmult)
-        alpha.est <- unlist(parm.est$alpha)
-        r.mu.boot <- parm.est$mu
-        
-        p.value.wald[m]  <- dm.wald(x=rx.dirmult, mu.x = r.mu.boot, alpha.null = r.null.dirmult, alpha.x=alpha.est)$p.value
-        p.value.lrt[m]   <- dm.lrt(x=rx.dirmult, mu.x = r.mu.boot, alpha.null = r.null.dirmult, alpha.x=alpha.est)$p.value
-        p.value.raptt[m] <- HotellingsT2(t(rx.raptt), mu = r.null.raptt, test = "chi")$p.value
-    }
-    c(mean(p.value.wald), mean(p.value.lrt), mean(p.value.raptt)) 
+    rx.dir    <- RP.prop %*% x.boot
+    rx.raptt  <- RP.orth %*% x.boot
+    
+    
+    r.null.theta <- RP.prop %*% theta.null
+    r.null.dir   <- log(r.null.theta) - log(r.null.theta[1]);
+    r.null.raptt <- RP.orth %*% theta.null
+    
+    
+    ## Estimate parameters with projected data
+    parm.est   <- dir.nr(rx.dir)
+    alpha.est  <- unlist(parm.est$alpha)
+    r.mu.boot  <- parm.est$mu
+    
+    p.value.wald[m]  <- dir.wald(x=rx.dir, mu.x = r.mu.boot, alpha = r.null.dir, alpha.x=alpha.est)$p.value
+    p.value.lrt[m]   <- dir.lrt(x=rx.dir, mu.x= r.mu.boot, alpha=r.null.dir, alpha.x=alpha.est)$p.value
+    p.value.raptt[m] <- HotellingsT2(t(rx.raptt), mu = r.null.raptt, test = "chi")$p.value
+  }
+  c(mean(p.value.wald), mean(p.value.lrt), mean(p.value.raptt)) 
 }
-
-raptt.emp.alpha  <- mean(mean.p[,3] < cut.off[3]); raptt.emp.alpha
-lrt.emp.alpha <- mean(mean.p[,2] < cut.off[2]); lrt.emp.alpha
-wald.emp.alpha <- mean(mean.p[,1] < cut.off[1]); wald.emp.alpha
-
-## WRITE THE SAVE FILE NAME INCLUDING THE VALUES PROVIDED IN ARGS
-## SOMETHING LIKE "Result_args[1]_args[2]_args[3].RData"
-#WRIET A TABLE
-emp.alpha <- cbind(raptt.emp.alpha, lrt.emp.alpha, wald.emp.alpha)
-colnames(emp.alpha)<- c("RAPTT", "LRT", "WALD")
 
 
 end.time <- Sys.time()
 
-running.time <- end.time - start.time; 
-running.time
-emp.alpha
+running.time <-  end.time - start.time; running.time
+wald.emp.alpha   <- mean(mean.p[,1] < cut.off[1]); wald.emp.alpha
+lrt.emp.alpha    <- mean(mean.p[,2] < cut.off[2]); lrt.emp.alpha
+raptt.emp.alpha  <- mean(mean.p[,3] < cut.off[3]); raptt.emp.alpha
+
+emp.alpha <- cbind(wald.emp.alpha, lrt.emp.alpha, raptt.emp.alpha)
+colnames(emp.alpha) <- c("wald","lrt", "raptt")
+# 
+write.csv(emp.alpha, file=paste0("/Users/bchoi/Google Drive/Projects/Random Project/Final version (042024)/results/Dir_OneSample_alpha_p_",p,"_n_",n,"_k_",k,"_mu_",mu,".csv") )
 ## WRITE THE SAVE FILE NAME INCLUDING THE VALUES PROVIDED IN ARGS
 ## SOMETHING LIKE "Result_args[1]_args[2]_args[3].RData"
-#save.image(file=paste0("DM_OneSample_alpha_p_", p,  "_n_", n, "_k_", k,  "_mu_", mu, ".RData"))
-#write.csv(emp.alpha, file=paste0("DM_OneSample_alpha_p_", p,  "_n_",  n, "_k_",  k, "_mu_", mu, ".csv"))
+
+#save.image(file="Dir_OneSample_alpha_p_args[1]_n_args[2]_k_args[3]_mu_args[4].RData")
+#save.image(file=paste0("Dir_OneSample_alpha_p_",p,"_n_",n,"_k_",k,"_mu_",mu,".RData"))
