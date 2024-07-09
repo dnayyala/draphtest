9e705322509a385e02aa8673b868a361bec856a0
Binnah <bnchoi.st@gamil.com>
2024-07-09 17:33:44 +0900

    Minor changes to build a package

--- R/dm_wald_test.R
+++ R/dm_wald_test.R
@@ -1,16 +1,15 @@
 dm.wald <- function(x, y, mu.x, mu.y = NULL, alpha.null = NULL, alpha.x, alpha.y = NULL,  type = "one"){
-    #' Wald-type test to parameter alpha
-    #' @param x A data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet-Multinomial distribution. 
-    #' @param y A data matrix of size $p \times n$ generated from [p]-dimensional Dirichlet-Multinomial distribution. 
-    #' @param mu.x A scalar parameter of x which represents the dispersion parameter 
-    #' @param mu.y A scalar parameter of y which represents the dispersion parameter 
-    #' @param alpha.null Vector of length p which represents the log-transformed mean vector under the null hypothesis.
+    #' Wald-type test for parameter alpha
+    #' @parm x A data matrix of size (p x n) generated from [p]-dimensional Dirichlet-Multinomial distribution. 
+    #' @parm y A data matrix of size (p x n) generated from [p]-dimensional Dirichlet-Multinomial distribution. 
+    #' @parm mu.x A scalar parameter of x which represents the dispersion parameter 
+    #' @parm mu.y A scalar parameter of y which represents the dispersion parameter 
+    #' @param alpha Vector of length p which represents the log-transformed mean vector under the null hypothesis.
     #' @param alpha.x Vector of length p which represents the log-transformed mean vector of x.  
     #' @param alpha.y Vector of length p which represents the log-transformed mean vector of y. 
     #' @param type If ```type``` is  set as ```one```, the function will return a one sample test result, else if ```type``` is  set as ```two``` then the function will return two samples test result.
     #' @return the function will return test statistic, p value and the result of the Wald-type test.
     
-   
     p <- nrow(x) 
     n <- ncol(x) 
     X.plus <- colSums(x)
