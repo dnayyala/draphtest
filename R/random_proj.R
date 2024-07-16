8e146cb351ef55e8f3b5a3b91bd388c14772c877
Deepak Nag Ayyala <deepaknagayyala@gmail.com>
2024-07-16 09:31:03 -0400

    Update random_proj.R
    Added single quote to be able to create the man file

--- R/random_proj.R
+++ R/random_proj.R
@@ -1,7 +1,7 @@
 
 random.proj <- function(nrow, ncol, seed=NULL){ 
-# Random matrix generator for Dirichlet distribution
-# 
+#' Random matrix generator for Dirichlet distribution
+#' 
 #' @param nrow Number of rows in the random matrix to be generated
 #' @param ncol Number of columns
 #' @param method The method to be used for generating elements in the matrix. If   
