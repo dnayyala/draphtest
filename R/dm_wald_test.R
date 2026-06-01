bf77f84069cc14b14b46859c5d87a86157465485
Choi <bchoi5@uth.tmc.edu>
2026-06-01 09:31:18 -0500

    Revised covariance matrix for each one-, two-sample test

--- R/dm_wald_test.R
+++ R/dm_wald_test.R
@@ -1,49 +1,45 @@
 dm.wald <- function(x, y, mu.x, mu.y = NULL, alpha.null = NULL, alpha.x, alpha.y = NULL,  type = "one"){
-    #' @title Wald-type test for parameter alpha
-    #' 
-    #' @description This function computes the Wald-type test statistic for testing equality of parameters of one and two sample Dirichlet-multinomial distribution
-    #' @param x A data matrix of size \eqn{p \times n} generated from [p]-dimensional Dirichlet-Multinomial distribution. 
-    #' @param y A data matrix of size \eqn{p \times n} generated from [p]-dimensional Dirichlet-Multinomial distribution. 
-    #' @param mu.x A scalar parameter of x which represents the dispersion parameter 
-    #' @param mu.y A scalar parameter of y which represents the dispersion parameter 
-    #' @param alpha.null Vector of length p which represents the log-transformed mean vector under the null hypothesis.
-    #' @param alpha.x Vector of length p which represents the log-transformed mean vector of x.  
-    #' @param alpha.y Vector of length p which represents the log-transformed mean vector of y. 
-    #' @param type If ```type``` is  set as ```one```, the function will return a one sample test result, else if ```type``` is  set as ```two``` then the function will return two samples test result.
-    #' @import stats
-    #' @export
-    #' @return the function will return test statistic, p value and the result of the Wald-type test.
-    
-    p <- nrow(x) 
-    n <- ncol(x) 
-    X.plus <- colSums(x)
-    N      <- mean(X.plus)  # Total sum 
-    
-    if (is.null(mu.y)){
-        type == "one"
-    } else {
-        type == "two"
-    }
-    
-    if (is.null(alpha.null)){
-        type == "two"
-    } else {
-        type == "one"
-    }
-    
-    if (type == "one"){
-        ## Check if data and parameter dimensions match
-        if (length(alpha.x) && length(alpha.null) != p){
-            stop("Data and parameter dimensions do not match.")
-        }
-        
-    fisher  <- -dm.hessian(x, mu, alpha.x, param = "alpha") # Fisher information matrix of data x 
-    diff    <- as.matrix(alpha.x[2:p] - alpha.null[2:p]) 
-
-    test.stat   <-  t(diff) %*% (fisher) %*% diff
-    p.value     <-  pchisq(test.stat, df=p-1, lower.tail=FALSE) 
+  #' @title Wald-type test for parameter alpha
+  #' 
+  #' @description This function computes the Wald-type test statistic for testing equality of parameters of one and two sample Dirichlet-multinomial distribution
+  #' @param x A data matrix of size \eqn{p \times n} generated from [p]-dimensional Dirichlet-Multinomial distribution. 
+  #' @param y A data matrix of size \eqn{p \times n} generated from [p]-dimensional Dirichlet-Multinomial distribution. 
+  #' @param mu.x A scalar parameter of x which represents the dispersion parameter 
+  #' @param mu.y A scalar parameter of y which represents the dispersion parameter 
+  #' @param alpha.null Vector of length p which represents the log-transformed mean vector under the null hypothesis.
+  #' @param alpha.x Vector of length p which represents the log-transformed mean vector of x.  
+  #' @param alpha.y Vector of length p which represents the log-transformed mean vector of y. 
+  #' @param type If ```type``` is  set as ```one```, the function will return a one sample test result, else if ```type``` is  set as ```two``` then the function will return two samples test result.
+  #' @import stats
+  #' @export
+  #' @return the function will return test statistic, p value and the result of the Wald-type test.
   
-    } else if (type == "two"){
+  p <- nrow(x) 
+  n <- ncol(x) 
+  
+  X.plus <- colSums(x)
+  N      <- mean(X.plus)  # Total sum 
+  
+  if (is.null(mu.y)){
+    type <- "one"
+  } else {
+    type <- "two"
+  }
+  
+  if (type == "one"){
+    ## Check if data and parameter dimensions match
+    if (length(alpha.x) && length(alpha.null) != p){
+      stop("Data and parameter dimensions do not match.")
+    }
+    
+    Hessian_a   <-  dm.hessian(x, mu.x, alpha.x, param = "alpha")  
+    diff        <-  as.matrix(alpha.x[2:p] - alpha[2:p])
+    
+    test.stat   <- (t(diff) %*%  -Hessian_a  %*% diff)
+    p.value     <- pchisq(test.stat, df=p-1, lower.tail=FALSE)
+    
+    
+  } else if (type == "two"){
     
     m <- ncol(y) 
     
@@ -51,18 +47,17 @@ dm.wald <- function(x, y, mu.x, mu.y = NULL, alpha.null = NULL, alpha.x, alpha.y
     M      <- mean(y.plus)  # Total sum 
     
     
-    fisher.x  <- - dm.hessian(x, mu.x, alpha.x, param = "alpha")# Fisher information matrix of data x
-    fisher.y  <- - dm.hessian(y, mu.y, alpha.y, param = "alpha")# Fisher information matrix of data x
+    H.x      <- dm.hessian(x, mu.x, alpha.x, param = "alpha") # Fisher information matrix of data x
+    H.y      <- dm.hessian(y, mu.y, alpha.y, param = "alpha") # Fisher information matrix of data x
     
-    est.cov.x     <-  solve(fisher.x)
-    est.cov.y     <-  solve(fisher.y)
-  
+    var.x     <-  solve(-H.x)
+    var.y     <-  solve(-H.y)
+    
     diff        <-  as.matrix(alpha.x[2:p] - alpha.y[2:p])
-  
-    test.stat   <-   t(diff) %*% solve(est.cov.x + est.cov.y) %*% diff
+    test.stat   <-   t(diff) %*% solve(var.x + var.y) %*% diff
     p.value     <- pchisq(test.stat, df=p-1, lower.tail=FALSE) 
-}
-    
+  }
+  
   result <- NULL;
   
   if (p.value < 0.05) {
